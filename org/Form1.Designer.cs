7af0a78655c9c040605aef176f2a1bafaa90b791
fran <fran@DESKTOP-F8LNKCF>
2022-05-14 17:46:00 +0200

    Aggiungere i file di progetto.

---
+++ org/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace org
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "08";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button1.Location = new System.Drawing.Point(72, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(402, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 519);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Ore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ org/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace org
+{
+    public partial class MainForm : Form
+    {
+        List<TextBox> tasks08 = new List<TextBox>(); //lista dei task delle 8
+        
+    
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // click sul bottone
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox task = new TextBox();
+            this.Controls.Add(task);
+            task.BringToFront();
+            task.Select();
+            task.Width = 20;
+            
+            if (tasks08.Count == 0)//primo textbox
+            {
+                task.Location = new Point(button1.Location.X, button1.Location.Y);                
+                task.TextChanged += new EventHandler(textBox_TextChanged);
+            }
+
+            else //textbox seguenti
+            {
+                int x = tasks08.Last().Location.X + tasks08.Last().Width + 2;
+                task.Location = new Point(x, button1.Location.Y);                
+                task.TextChanged += new EventHandler(textBox_TextChanged);
+            }
+            tasks08.Add(task);//aggiungi a lista, serve a vedere dov'è l'ultimo task
+
+        }
+
+        // Handler TextChanged: resize delle textbox quando cambia il testo
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox task = (TextBox)sender;
+
+            Size size = TextRenderer.MeasureText(task.Text, task.Font);
+            task.Width = size.Width + 5;
+            //task.Height = size.Height;
+            if (task.Text == "") //in teoria elimina task
+            {
+                //task.Dispose();
+            }
+
+            //sposta i task che sono a destra di quello che sta cambiando dimensione
+            List<TextBox> temp = new List<TextBox>();
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    TextBox tb = (TextBox)ctrl;
+                    if (tb.Location.X > task.Location.X)
+                    {
+                        temp.Add(tb);
+                        //int x = tb.Location.X + task.Width;
+                        //tb.Location = new Point(x+1, tb.Location.Y); 
+                    }
+
+                    List<TextBox> SortedList = temp.OrderBy(o => o.Location.X).ToList();
+                    SortedList[0].Location = new Point(task.Location.X + task.Width, task.Location.Y);
+                    for (int i=1; i < temp.Count(); i++)
+                    {
+                        SortedList[i].Location = new Point(SortedList[i - 1].Location.X + SortedList[i - 1].Width, task.Location.Y);
+                    }
+                }
+            }
+            
+        }
+    }
+}
